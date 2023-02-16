a6b46d09e1585636867fcc644678d11841859c3a
“Aselorm7” <Chencharis31@gmail.com>
2023-02-16 12:17:36 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - My program that prints a line with puts function
+ *Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
